f74ee3655b7ad48ca48ba43308a86490bbf3ca18
vasiltatarov <vasiltatarov3@gmail.com>
2021-05-17 19:33:17 +0300

    deploy

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,13 +3,13 @@ import { Button } from './Button';
 import './HeroSection.css';
 import '../App.css';
 // import video1 from '../assets/video-1.mp4';
-// import video2 from '../assets/video-2.mp4';
+import video2 from '../assets/video-2.mp4';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
             {/* <video src={video1} autoPlay loop muted /> */}
-            {/* <video src={video2} autoPlay loop muted /> */}
+            <video src={video2} autoPlay loop muted />
             <h1>The Traveler's Diaries</h1>
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
