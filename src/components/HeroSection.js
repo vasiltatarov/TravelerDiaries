5a1c5a14c8db8a0af10aad993162413059876ba2
vasiltatarov <vasiltatarov3@gmail.com>
2021-05-17 18:57:22 +0300

    try to fix video on home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Button } from './Button';
 import './HeroSection.css';
 import '../App.css';
+// import video1 from '../assets/video-1.mp4';
+import video2 from '../assets/video-2.mp4';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
-            {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
-            <video src='/videos/video-1.mp4' autoPlay loop muted />
+            {/* <video src={video1} autoPlay loop muted /> */}
+            <video src={video2} autoPlay loop muted />
             <h1>The Traveler's Diaries</h1>
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
